4cd7da97e40a842c7e8eb09f924216ddb1c3aab1
Alfonso Vásquez <avasquez@rivetlogic.com>
2014-05-07 15:58:57 -0600

    Added SetUtils.

---
+++ utilities/src/main/java/org/craftercms/commons/collections/SetUtils.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2007-2014 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.commons.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Utilities for the {@link java.util.Set} collection.
+ *
+ * @author avasquez
+ */
+public class SetUtils {
+
+    private SetUtils() {
+    }
+
+    /**
+     * Creates a set from the array elements.
+     *
+     * @param array the array with the elements
+     *
+     * @return the set with the elements
+     */
+    public static <T> Set<T> asSet(T... array) {
+        Set<T> set = null;
+
+        if (array != null) {
+            set = new HashSet<>(array.length);
+            for (T element : array) {
+                set.add(element);
+            }
+        }
+
+        return set;
+    }
+
+}
