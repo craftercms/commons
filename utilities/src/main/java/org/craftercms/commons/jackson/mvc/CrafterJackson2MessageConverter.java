8667fe34204400f85c28f18d2a404819c867f7aa
Carlos Ortiz <carlos.ortiz@craftercms.org>
2014-06-25 11:37:11 -0600

    Injection of Beans @jackson Serialization

--- utilities/src/main/java/org/craftercms/commons/jackson/mvc/CrafterJackson2MessageConverter.java
+++ utilities/src/main/java/org/craftercms/commons/jackson/mvc/CrafterJackson2MessageConverter.java
@@ -20,14 +20,22 @@ package org.craftercms.commons.jackson.mvc;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.craftercms.commons.jackson.mvc.annotations.InjectValue;
+import org.craftercms.commons.jackson.mvc.annotations.InjectValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -37,6 +45,9 @@ public class CrafterJackson2MessageConverter extends MappingJackson2HttpMessageC
 
     protected String jsonPrefix;
     protected FilterProvider filter;
+    protected InjectValueFactory injectValueFactory;
+    private Logger log = LoggerFactory.getLogger(CrafterJackson2MessageConverter.class);
+
 
     @Override
     protected void writeInternal(Object object, HttpOutputMessage outputMessage) throws IOException,
@@ -44,7 +55,7 @@ public class CrafterJackson2MessageConverter extends MappingJackson2HttpMessageC
 
         JsonEncoding encoding = getJsonEncoding(outputMessage.getHeaders().getContentType());
         JsonGenerator jsonGenerator = this.getObjectMapper().getFactory().createGenerator(outputMessage.getBody(),
-            encoding);
+    encoding);
         // A workaround for JsonGenerators not applying serialization features
         // https://github.com/FasterXML/jackson-databind/issues/12
         if (this.getObjectMapper().isEnabled(SerializationFeature.INDENT_OUTPUT)) {
@@ -55,9 +66,70 @@ public class CrafterJackson2MessageConverter extends MappingJackson2HttpMessageC
             if (this.jsonPrefix != null) {
                 jsonGenerator.writeRaw(this.jsonPrefix);
             }
-            this.getObjectMapper().writer(filter).writeValue(jsonGenerator, object);
+            if (injectValueFactory != null) {
+                injectValues(object);
+            }
+            ObjectWriter writer = this.getObjectMapper().writer(filter);
+            writer.writeValue(jsonGenerator, object);
         } catch (JsonProcessingException ex) {
             throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
+        }
+    }
+
+    private void injectValues(final Object object) {
+        try {
+            PropertyDescriptor[] propertiesDescriptor = PropertyUtils.getPropertyDescriptors(object);
+            for (PropertyDescriptor propertyDescriptor : propertiesDescriptor) {
+                // Avoid the "getClass" as a property
+                if(propertyDescriptor.getPropertyType().equals(Class.class) || (propertyDescriptor.getReadMethod()
+                    ==null && propertyDescriptor.getWriteMethod()==null)){
+                    continue;
+                }
+                Field field = getAllFieldsFor(object.getClass(), propertyDescriptor.getName());
+                if (field.isAnnotationPresent(InjectValue.class)) {
+                    injectValue(object, field);
+                   continue;
+                }
+                Object fieldValue = PropertyUtils.getProperty(object, propertyDescriptor.getName());
+                if (Iterable.class.isInstance(fieldValue)) {
+                    for (Object collectionObject : (Iterable)fieldValue) {
+                        injectValues(collectionObject);
+                    }
+                } else if (Iterator.class.isInstance(fieldValue)) {
+                    Iterator iter = (Iterator)fieldValue;
+                    while (iter.hasNext()) {
+                        injectValues(iter.next());
+                    }
+                }
+            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | NoSuchFieldException e) {
+            log.error("Unable to inject Value for class " + object.getClass(), e);
+        }
+    }
+
+
+    private Field getAllFieldsFor(final Class<?> object, final String fieldName) throws NoSuchFieldException {
+        if (object != null) {
+            try {
+                return object.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                return getAllFieldsFor(object.getSuperclass(), fieldName);
+            }
+        }
+        throw new NoSuchFieldException("Field " + fieldName + " does not exist");
+    }
+
+    private void injectValue(final Object object, final Field field) {
+        //Should be null due we ask before if the annotation exists !!
+        String propertyToUseName = field.getAnnotation(InjectValue.class).useProperty();
+        try {
+            Object propertyValue = PropertyUtils.getProperty(object, propertyToUseName);
+            Object valueToInject = injectValueFactory.getObjectFor(PropertyUtils.getPropertyType(object,
+                field.getName()), propertyValue, object);
+            PropertyUtils.setProperty(object, field.getName(), valueToInject);
+
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            log.error("Unable to inject Value " + field.getName() + "for class " + object.getClass(), e);
         }
     }
 
@@ -73,5 +145,10 @@ public class CrafterJackson2MessageConverter extends MappingJackson2HttpMessageC
     @Required()
     public void setFilter(final FilterProvider filter) {
         this.filter = filter;
+    }
+
+
+    public void setInjectValueFactory(final InjectValueFactory injectValueFactory) {
+        this.injectValueFactory = injectValueFactory;
     }
 }
