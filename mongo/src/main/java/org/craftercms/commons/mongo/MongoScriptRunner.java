f23492a64e083410e0d715d374ee5a969a8e06fa
Carlos Ortiz <carlos.ortiz@craftercms.org>
2015-09-25 21:36:56 -0600

    CRAFTERCMS-1446 MongoScriptRunner is using eval to execute scripts http://wiki.craftercms.org/pages/viewpage.action?pageId=3867032#CrafterProfile/SocialExpress-InstallationInstructions-MongoDbconfigurationanddatabootstrappingGreen2.5.1onwards

--- mongo/src/main/java/org/craftercms/commons/mongo/MongoScriptRunner.java
+++ mongo/src/main/java/org/craftercms/commons/mongo/MongoScriptRunner.java
@@ -19,11 +19,27 @@ package org.craftercms.commons.mongo;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.codec.net.URLCodec;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
@@ -33,6 +49,8 @@ import org.springframework.core.io.Resource;
 import com.mongodb.CommandResult;
 import com.mongodb.DB;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoURI;
 
 /**
  * Utility class for running Mongo scripts in JS.
@@ -49,6 +67,10 @@ public class MongoScriptRunner {
     private String password;
     private List<Resource> scriptPaths;
     private boolean runOnInit;
+    private boolean useMongoClient;
+    private String mongoClientBin;
+    private String connectionStr;
+
 
     public MongoScriptRunner() {
         this.runOnInit = true;
@@ -81,18 +103,63 @@ public class MongoScriptRunner {
         this.runOnInit = runOnInit;
     }
 
+    public void setUseMongoClient(final boolean useMongoClient) {
+        this.useMongoClient = useMongoClient;
+    }
+
+    public void setMongoClientBin(final String mongoClientBin) {
+        this.mongoClientBin = mongoClientBin;
+    }
+
+    public void setConnectionStr(final String connectionStr) {
+        this.connectionStr = connectionStr;
+    }
+
     @PostConstruct
-    public void init() throws MongoDataException {
+    public void init()  {
+
         if (runOnInit) {
-            runScripts();
+            if(useMongoClient){
+                runScriptsWithMongoClient();
+            }else {
+                runScripts();
+            }
         }
     }
 
-    public void runScripts() throws MongoDataException {
-        DB db = getDB();
+    private void runScriptsWithMongoClient() {
+        List<String> toExecute=new ArrayList<>();
 
-        for (Resource scriptPath : scriptPaths) {
-            runScript(db, scriptPath);
+        try {
+            for (Resource scriptPath : scriptPaths) {
+                if (scriptPath.getFile().isDirectory()) {
+                    Files.walkFileTree(scriptPath.getFile().toPath(), new JSFileVisitor(toExecute));
+                }else{
+                    toExecute.add(scriptPath.getFile().getAbsolutePath());
+                }
+            }
+            final Path allScripsFile = Files.createTempFile("ScriptRunner", ".js");
+            StringBuilder builder =new StringBuilder();
+            for (String path : toExecute) {
+                builder.append(String.format("load('%s');\n",path));
+            }
+            FileUtils.writeStringToFile(allScripsFile.toFile(),builder.toString(),"UTF-8");
+            runScript(allScripsFile);
+            Files.deleteIfExists(allScripsFile);
+        }catch (IOException | MongoDataException ex){
+            logger.error("Unable to run script using MongoClient",ex);
+        }
+    }
+
+    public void runScripts() {
+        try {
+            DB db = getDB();
+            for (Resource scriptPath : scriptPaths) {
+                runScript(db, scriptPath);
+            }
+        }catch (Exception ex){
+            //We where unable to run script due some reason , logged and make sure the app at least startup.
+            logger.error("Unable to run scripts due a internal exception ",ex);
         }
     }
 
@@ -132,6 +199,73 @@ public class MongoScriptRunner {
         }
     }
 
+    private void runScript(Path scriptPath) throws MongoDataException {
+        String script;
+        try {
+            ProcessBuilder mongoProcess = new ProcessBuilder(getCommands(scriptPath));
+            Process process=mongoProcess.start();
+            int result=process.waitFor();
+            String stdOut;
+            String errOut;
+            try(ByteArrayOutputStream out = new ByteArrayOutputStream()){
+                IOUtils.copy(process.getInputStream(),out);
+                stdOut = new String(out.toByteArray(),"UTF-8");
+                out.reset();
+                IOUtils.copy(process.getErrorStream(),out);
+                errOut = new String(out.toByteArray(),"UTF-8");
+                IOUtils.copy(process.getInputStream(),out);
+            }
+            if(result!=0){
+                throw new IOException("Process return error \n std out:"+stdOut+"\n err out: \n"+errOut);
+            }else{
+             logger.debug("Process return \n std out:"+stdOut+"\n err out: \n"+errOut);
+            }
+        } catch (IOException | InterruptedException ex) {
+            logger.error("Unable to Execute mongo Process", ex);
+        }
+    }
+
+    private List<String> getCommands(final Path scriptPath) throws MongoDataException {
+        List<String> commandList=new ArrayList<>();
+        if(SystemUtils.IS_OS_WINDOWS){
+            commandList.add("CMD");
+            commandList.add("/C");
+        }
+        if(StringUtils.isBlank(mongoClientBin)){
+            throw new MongoDataException("Unable to run scripts, mongo client bin path is not set ");
+        }
+        String pwd = null;
+        String authSource=null;
+        String user=null;
+        MongoClientURI uri=new MongoClientURI(connectionStr);
+        if(uri.getCredentials()!=null) {
+             authSource= uri.getCredentials().getSource();
+             user= uri.getCredentials().getUserName();
+            if (uri.getCredentials().getPassword() != null) {
+                pwd = new String(uri.getCredentials().getPassword());
+            }
+        }
+        String replicaSetName="";
+        if(uri.getHosts().size()>1){
+            replicaSetName=uri.getOptions().getRequiredReplicaSetName()+"/";
+        }
+        final String host = StringUtils.trim(replicaSetName+StringUtils.join(uri.getHosts(),","));
+        commandList.add(mongoClientBin);
+        commandList.add("--host");
+        commandList.add(host);
+        commandList.add(uri.getDatabase());
+        if(StringUtils.isNotBlank(user) && StringUtils.isNotBlank(pwd) && StringUtils.isNotBlank(authSource)) {
+            commandList.add("-u");
+            commandList.add(user);
+            commandList.add("-p");
+            commandList.add(pwd);
+            commandList.add("--authenticationDatabase");
+            commandList.add(authSource);
+        }
+        commandList.add(scriptPath.toAbsolutePath().toString());
+        return commandList;
+    }
+
     private DB getDB() throws MongoDataException {
         DB db = mongo.getDB(dbName);
         if (!StringUtils.isBlank(password)) {
@@ -143,4 +277,20 @@ public class MongoScriptRunner {
         return db;
     }
 
+
+    class JSFileVisitor extends SimpleFileVisitor<Path> {
+        private List<String> filesTOAdd;
+
+        public JSFileVisitor(final List<String> filesTOAdd) {
+            this.filesTOAdd = filesTOAdd;
+        }
+
+        @Override
+        public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+            if(attrs.isRegularFile() && attrs.size()>0 && StringUtils.endsWithIgnoreCase(file.toString(),".js")){
+                filesTOAdd.add(file.toAbsolutePath().toString());
+            }
+            return super.visitFile(file, attrs);
+        }
+    }
 }
