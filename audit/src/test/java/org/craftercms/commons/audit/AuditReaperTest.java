fd3dc946f74a8c63b1a01cc05ee669ba9b2ff791
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-11-08 14:53:28 -0600

    Hopefully now it does solves 1272

--- audit/src/test/java/org/craftercms/commons/audit/AuditReaperTest.java
+++ audit/src/test/java/org/craftercms/commons/audit/AuditReaperTest.java
@@ -25,6 +25,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+@SuppressWarnings("unchecked")
 public class AuditReaperTest {
 
     private TestAuditServiceImpl auditService;
