ebad71c86fd1ddfef82e2fd4d4c94a4dc7c93bc6
Jose Ross <jdross17@gmail.com>
2020-08-14 16:50:40 -0600

    Improvements for locale support

--- utilities/src/main/java/org/craftercms/commons/locale/LocaleUtils.java
+++ utilities/src/main/java/org/craftercms/commons/locale/LocaleUtils.java
@@ -17,12 +17,20 @@ package org.craftercms.commons.locale;
 
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections4.ListUtils.union;
+import static org.apache.commons.io.FilenameUtils.getBaseName;
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.io.FilenameUtils.getFullPath;
+import static org.apache.commons.lang3.LocaleUtils.localeLookupList;
 import static org.apache.commons.lang3.StringUtils.isAnyEmpty;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.apache.commons.lang3.StringUtils.substringAfter;
 import static org.apache.commons.lang3.StringUtils.substringBefore;
+import static org.apache.commons.lang3.StringUtils.substringBeforeLast;
 
 /**
  * Utility class for handling locale codes
@@ -34,10 +42,10 @@ public abstract class LocaleUtils {
 
     public static final String CONFIG_KEY_DEFAULT_LOCALE = "defaultLocaleCode";
     public static final String CONFIG_KEY_SUPPORTED_LOCALES = "localeCodes.localeCode";
-    public static final String CONFIG_KEY_LOCALE_RESOLVER = "localeResolvers.localeResolver";
     public static final String CONFIG_KEY_FALLBACK = "fallbackToDefaultLocale";
 
     public static final String LOCALE_SEPARATOR = "_";
+    public static final Pattern LOCALE_PATTERN = Pattern.compile("_(\\w\\w(?:_\\w\\w)?)\\.");
 
     public static Locale parseLocale(String localeValue) {
         Locale locale = null;
@@ -57,7 +65,7 @@ public abstract class LocaleUtils {
     }
 
     public static List<Locale> parseLocales(List<String> localeValues) {
-        return localeValues.stream().map(LocaleUtils::parseLocale).collect(Collectors.toList());
+        return localeValues.stream().map(LocaleUtils::parseLocale).collect(toList());
     }
 
     public static String toString(Locale locale) {
@@ -71,6 +79,54 @@ public abstract class LocaleUtils {
 
     public static String appendLocale(String str, Locale locale) {
         return String.format("%s-%s", str, toString(locale));
+    }
+
+    public static String localizePath(String path, Locale locale) {
+        return getFullPath(path) + getBaseName(path) + LOCALE_SEPARATOR +
+                LocaleUtils.toString(locale) + "." + getExtension(path);
+    }
+
+    public static String delocalizePath(String path) {
+        var matcher = LOCALE_PATTERN.matcher(path);
+        if (matcher.find()) {
+            var localeValue = matcher.group(1);
+            try {
+                var locale = parseLocale(localeValue);
+                if (locale != null) {
+                    return getFullPath(path) + substringBeforeLast(getBaseName(path), LOCALE_SEPARATOR) +
+                            "." + getExtension(path);
+                }
+            } catch (IllegalArgumentException e) {
+                // invalid locale code, ignore
+            }
+        }
+        return path;
+    }
+
+    public static String findPath(String path, Locale primary, Locale fallback, Predicate<String> exists) {
+        return getCompatibleLocales(primary, fallback).stream()
+                .map(locale -> localizePath(path, locale))
+                .filter(exists)
+                .findFirst()
+                .orElse(path);
+    }
+
+    public static List<String> appendLocales(String name, Locale primary, Locale fallback) {
+        return getCompatibleLocales(primary, fallback).stream()
+                .map(locale -> appendLocale(name, locale))
+                .collect(toList());
+    }
+
+    public static List<Locale> getCompatibleLocales(Locale primary, Locale fallback) {
+        if (fallback != null) {
+            return union(localeLookupList(primary), localeLookupList(fallback));
+        } else {
+            return localeLookupList(primary);
+        }
+    }
+
+    public static List<Locale> getCompatibleLocales(Locale locale) {
+        return getCompatibleLocales(locale, null);
     }
 
 }
