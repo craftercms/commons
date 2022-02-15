d5f06c7ed98f5c6736a61ca4e369f996aa1d92ec
Sumer Jabri <sumer.jabri@craftercms.org>
2022-02-15 16:26:45 -0500

    Misc updates.

--- entitlements/src/main/java/org/craftercms/commons/entitlements/validator/impl/DefaultEntitlementValidatorImpl.java
+++ entitlements/src/main/java/org/craftercms/commons/entitlements/validator/impl/DefaultEntitlementValidatorImpl.java
@@ -36,11 +36,7 @@ public class DefaultEntitlementValidatorImpl implements EntitlementValidator, In
 
     private static final Logger logger = LoggerFactory.getLogger(DefaultEntitlementValidatorImpl.class);
 
-    private static final String DESCRIPTION = "UG93ZXJlZCBieSBDcmFmdGVyIENNUyB2JHt2ZXJzaW9ufS4gQ2hlY2sgaXQgb3V0IDxhIGhyZWY9Imh"
-        + "0dHBzOi8vY3JhZnRlcmNtcy5vcmciIHRhcmdldD0iX2JsYW5rIj5oZXJlPC9hPi4gUmVwb3J0IGEgPGEgaH"
-        + "JlZj0iaHR0cHM6Ly9naXRodWIuY29tL2NyYWZ0ZXJjbXMvY3JhZnRlcmNtcy9pc3N1ZXMiIHRhcmdldD0i"
-        + "X2JsYW5rIj5idWc8L2E+LiA8YSBocmVmPSJodHRwczovL2NyYWZ0ZXJjbXMub3JnL2Jsb2ciIHRhcmdld"
-        + "D0iX2JsYW5rIj5DcmFmdGVyIE5ld3M8L2E+Lg==";
+    private static final String DESCRIPTION = "UG93ZXJlZCBieSBDcmFmdGVyQ01TIHYke3ZlcnNpb259LiBDaGVjayBpdCBvdXQgPGEgaHJlZj0iaHR0cHM6Ly9jcmFmdGVyY21zLm9yZyIgdGFyZ2V0PSJfYmxhbmsiPmhlcmU8L2E+LiBSZXBvcnQgYSA8YSBocmVmPSJodHRwczovL2dpdGh1Yi5jb20vY3JhZnRlcmNtcy9jcmFmdGVyY21zL2lzc3VlcyIgdGFyZ2V0PSJfYmxhbmsiPmJ1ZzwvYT4uIDxhIGhyZWY9Imh0dHBzOi8vY3JhZnRlcmNtcy5vcmcvYmxvZyIgdGFyZ2V0PSJfYmxhbmsiPkNyYWZ0ZXIgTmV3czwvYT4u"
 
     public void afterPropertiesSet() {
         logger.info("Using CrafterCMS Community Edition");
