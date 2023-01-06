5d1c3abd74b43d5bde068b1724329f16100df5bb
Jonathan Méndez <jonnathan.mendez@gmail.com>
2023-01-06 13:42:34 -0600

    GROUP_NAME regex. Update USERNAME for minimum length (#323)
    * Adding support for Collection parameter validation
    Default validators targetKey to the parameter name
    
    * Adding support for Object validator
    
    * Remove unused import
    
    * Prevent comma in SQL_ORDER_BY param
    
    * GROUP_NAME regex. Update USERNAME for minimum length
    
    * Allow dashes in user and group names

--- validation/src/main/java/org/craftercms/commons/validation/annotations/param/EsapiValidationType.java
+++ validation/src/main/java/org/craftercms/commons/validation/annotations/param/EsapiValidationType.java
@@ -26,6 +26,7 @@ public enum EsapiValidationType {
     SITE_ID("SITEID"),
     EMAIL("EMAIL"),
     USERNAME("USERNAME"),
+    GROUP_NAME("GROUP_NAME"),
     SEARCH_KEYWORDS("SEARCH_KEYWORDS"),
     SQL_ORDER_BY("SQL_ORDER_BY");
 
