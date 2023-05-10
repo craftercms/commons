cedca8768603bb1cf9bd15c7985618600f2dd75e
Jonathan Mendez <jonnathan.mendez@gmail.com>
2023-05-10 09:26:50 -0600

    Relax CONTENT_PATH_WRITE regex, remove unused ASSET_PATH_WRITE

--- validation/src/test/java/org/craftercms/commons/validation/validators/impl/ContentNewPathValidatorTest.java
+++ validation/src/test/java/org/craftercms/commons/validation/validators/impl/ContentNewPathValidatorTest.java
@@ -33,7 +33,17 @@ public class ContentNewPathValidatorTest implements ValidatorTest {
 
     @Test
     public void testSpace() {
-        assertRejected("site/website/folder 1/index.xml");
+        assertValid("site/website/folder 1/index.xml");
+    }
+
+    @Test
+    public void testSpace2() {
+        assertValid("static-assets/images/my picture.png");
+    }
+
+    @Test
+    public void testSpace3() {
+        assertValid("johnny mnemonic");
     }
 
     @Test
@@ -44,11 +54,6 @@ public class ContentNewPathValidatorTest implements ValidatorTest {
     @Test
     public void testStartWithDigit2() {
         assertValid("7s1t3s/and/more");
-    }
-
-    @Test
-    public void testSpaces() {
-        assertRejected("johnny mnemonic");
     }
 
     @Test
@@ -83,7 +88,7 @@ public class ContentNewPathValidatorTest implements ValidatorTest {
 
     @Test
     public void testMixedCase() {
-        assertRejected("/site/website/DOCS/to/content/index.xml");
+        assertValid("/site/website/DOCS/to/content/index.xml");
     }
 
     @Test
