3e3ea6267264f8cf97451221f47684d763d77078
Phil Nguyen <nquangphuong@gmail.com>
2024-05-20 17:07:34 +0700

    Use executor to copy async

--- utilities/src/main/java/org/craftercms/commons/aws/AwsUtils.java
+++ utilities/src/main/java/org/craftercms/commons/aws/AwsUtils.java
@@ -24,8 +24,11 @@ import software.amazon.awssdk.transfer.s3.model.CompletedCopy;
 import software.amazon.awssdk.transfer.s3.model.Copy;
 import software.amazon.awssdk.transfer.s3.model.CopyRequest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Provides utility methods for AWS services
@@ -45,37 +48,42 @@ public final class AwsUtils {
     /**
      * Copy a list of objects from a source bucket to a target bucket
      *
-     * @param client          The {@link S3AsyncClient} client
-     * @param sourceBucket    The source bucket
-     * @param sourceBaseKey   The base key in the source bucket (i.e. prefix for all paths)
-     * @param targetBucket    The target bucket
-     * @param targetBaseKey   The base key in the target bucket (i.e. prefix for all paths)
-     * @param paths           The list of paths to copy
+     * @param client                The {@link S3AsyncClient} client
+     * @param threadPoolExecutor    The thread pool executor
+     * @param sourceBucket          The source bucket
+     * @param sourceBaseKey         The base key in the source bucket (i.e. prefix for all paths)
+     * @param targetBucket          The target bucket
+     * @param targetBaseKey         The base key in the target bucket (i.e. prefix for all paths)
+     * @param paths                 The list of paths to copy
      */
-    public static void copyObjects(S3AsyncClient client, String sourceBucket, String sourceBaseKey,
+    public static void copyObjects(S3AsyncClient client, ThreadPoolExecutor threadPoolExecutor, String sourceBucket, String sourceBaseKey,
                                    String targetBucket, String targetBaseKey, List<String> paths) {
 
         S3TransferManager transferManager = buildTransferManager(client);
         try {
             logger.debug("Copying {} objects from '{}' to '{}'", paths.size(), sourceBucket, targetBucket);
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
             for (String path : paths) {
-                logger.debug("Copying '{}' from '{}' to '{}'", path, sourceBucket, targetBucket);
-                String sourceKey = sourceBaseKey + path;
-                String targetKey = targetBaseKey + path;
-                CopyObjectRequest copyObjectRequest = CopyObjectRequest.builder()
-                        .sourceBucket(sourceBucket)
-                        .sourceKey(sourceKey)
-                        .destinationBucket(targetBucket)
-                        .destinationKey(targetKey)
-                        .build();
-                CopyRequest copyRequest = CopyRequest.builder()
-                        .copyObjectRequest(copyObjectRequest)
-                        .build();
-                Copy copy = transferManager.copy(copyRequest);
-                CompletedCopy completedCopy = copy.completionFuture().join();
-                logger.debug("Finished copying '{}' from '{}' to '{}' with result '{}'", path, sourceBucket, targetBucket,
-                        completedCopy.response().copyObjectResult());
+                futures.add(CompletableFuture.runAsync(() -> {
+                    logger.debug("Copying '{}' from '{}' to '{}'", path, sourceBucket, targetBucket);
+                    String sourceKey = sourceBaseKey + path;
+                    String targetKey = targetBaseKey + path;
+                    CopyObjectRequest copyObjectRequest = CopyObjectRequest.builder()
+                            .sourceBucket(sourceBucket)
+                            .sourceKey(sourceKey)
+                            .destinationBucket(targetBucket)
+                            .destinationKey(targetKey)
+                            .build();
+                    CopyRequest copyRequest = CopyRequest.builder()
+                            .copyObjectRequest(copyObjectRequest)
+                            .build();
+                    Copy copy = transferManager.copy(copyRequest);
+                    CompletedCopy completedCopy = copy.completionFuture().join();
+                    logger.debug("Finished copying '{}' from '{}' to '{}' with result '{}'", path, sourceBucket, targetBucket,
+                            completedCopy.response().copyObjectResult());
+                }, threadPoolExecutor));
             }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
             logger.debug("Finished copying {} objects from '{}' to '{}'", paths.size(), sourceBucket, targetBucket);
         } finally {
             transferManager.close();
