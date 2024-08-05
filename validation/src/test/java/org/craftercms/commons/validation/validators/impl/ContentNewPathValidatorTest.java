c45961c35ca83006d6336ec111d6ed2c19aab374
Phil Nguyen <nquangphuong@gmail.com>
2024-08-05 22:36:31 +0700

    Update path validate regex to prevent relative path directory traversal (#391)
    * Update path validate regex to prevent relative path directory traversal
    
    * Allow curly braces in content path
    
    * Support curly braces at start of path

--- validation/src/test/java/org/craftercms/commons/validation/validators/impl/ContentNewPathValidatorTest.java
+++ validation/src/test/java/org/craftercms/commons/validation/validators/impl/ContentNewPathValidatorTest.java
@@ -101,6 +101,41 @@ public class ContentNewPathValidatorTest implements ValidatorTest {
         assertRejected("/site/website/folder#hash/to/content/index.xml");
     }
 
+    @Test
+    public void testInvalidPathTraversal() {
+        assertRejected("../../site/website");
+        assertRejected("./site/website");
+        assertRejected("/site/website/../../sample");
+    }
+
+    @Test
+    public void testValidCurlyBraces() {
+        assertValid("/scripts/rest/{version}");
+        assertValid("/site/website/{version}/index.xml");
+        assertValid("/site/{a}/{b}/{c}/sample.xml");
+        assertValid("/site/component/{abc}-sample.xml");
+        assertValid("/site/component/{version}");
+        assertValid("/site/component/{version.xml}/abc/xyz");
+        assertValid("/site/component/{version}");
+        assertValid("/site/component/{a.sample.version}");
+        assertValid("/site/a.sample.component/{version}");
+        assertValid("/site/component/{index.xml}");
+        assertValid("{version}");
+        assertValid("{version}/{version}/component/sample/{new.version}/index.xml");
+        assertValid("{version}/component");
+    }
+
+    @Test
+    public void testInvalidCurlyBraces() {
+        assertRejected("/site/website/{version");
+        assertRejected("/site/website/version}");
+        assertRejected("/site/website/{{version}}");
+        assertRejected("/site/website/{version}/sample/{newVersion");
+        assertRejected("{{version}}");
+        assertRejected("{version/sample/path}");
+        assertRejected("/sample/path/{with{wrong{curl{brances}}}");
+    }
+
     @Override
     public Validator getValidator() {
         return validator;
