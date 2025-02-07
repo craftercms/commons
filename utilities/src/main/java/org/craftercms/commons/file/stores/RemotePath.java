b50602fed42ca9deb3430617dfeb5574ac7a0002
Phil Nguyen <nquangphuong@gmail.com>
2025-02-08 02:51:55 +0700

    Remove Box Support (#401)

--- utilities/src/main/java/org/craftercms/commons/file/stores/RemotePath.java
+++ utilities/src/main/java/org/craftercms/commons/file/stores/RemotePath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -31,7 +31,7 @@ public class RemotePath {
     }
 
     /**
-     * Returns the remote store type (s3, box, etc).
+     * Returns the remote store type (s3, etc).
      */
     public String getStoreType() {
         return storeType;
