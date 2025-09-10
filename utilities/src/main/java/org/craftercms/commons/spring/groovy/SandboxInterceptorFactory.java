8ce8660810d9ee76e1c1d8c1f8a6365221a905ae
Jonathan Méndez <jonnathan.mendez@gmail.com>
2025-09-10 13:18:09 -0600

    Add support for groovy sandbox whitelist (#413)
    * Add support for groovy sandbox whitelist
    
    * Validate constructor parameters

--- utilities/src/main/java/org/craftercms/commons/spring/groovy/SandboxInterceptorFactory.java
+++ utilities/src/main/java/org/craftercms/commons/spring/groovy/SandboxInterceptorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -15,17 +15,25 @@
  */
 package org.craftercms.commons.spring.groovy;
 
+import org.jenkinsci.plugins.scriptsecurity.sandbox.Whitelist;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.blacklists.Blacklist;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SandboxInterceptor;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.CompositeWhitelist;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.PermitAllWhitelist;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.StaticWhitelist;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 import org.springframework.core.io.Resource;
 
 import java.beans.ConstructorProperties;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
 
 /**
  * Implementation of {@link org.springframework.beans.factory.FactoryBean} for {@link SandboxInterceptor}
@@ -35,54 +43,113 @@ import java.util.List;
  */
 public class SandboxInterceptorFactory extends AbstractFactoryBean<SandboxInterceptor> {
 
-    /**
-     * Indicates if the sandbox should be enabled
-     */
-    protected boolean sandboxEnabled;
+	/**
+	 * Indicates if the sandbox should be enabled
+	 */
+	protected final boolean sandboxEnabled;
 
-    /**
-     * Indicates if the blacklist should be enabled
-     */
-    protected final boolean blacklistEnabled;
+	/**
+	 * Indicates if the blacklist should be enabled
+	 */
+	protected final boolean blacklistEnabled;
 
-    /**
-     * Resource containing the restrictions
-     */
-    protected Resource blacklist;
+	/**
+	 * Resource containing the restrictions
+	 */
+	protected final Resource blacklist;
 
-    /**
-     * List of regex to allow System.getenv()
-     */
-    protected List<String> whitelistGetEnvRegex;
 
-    @ConstructorProperties({"sandboxEnabled", "blacklistEnabled", "blacklist", "whitelistGetEnvRegex"})
-    public SandboxInterceptorFactory(boolean sandboxEnabled, boolean blacklistEnabled, Resource blacklist, final String[] whitelistGetEnvRegex) {
-        this.sandboxEnabled = sandboxEnabled;
-        this.blacklistEnabled = blacklistEnabled;
-        this.blacklist = blacklist;
-        this.whitelistGetEnvRegex = Arrays.stream(whitelistGetEnvRegex).toList();
-    }
+	/**
+	 * Indicates if the whitelist should be enabled
+	 */
+	protected final boolean whitelistEnabled;
 
-    @Override
-    public Class<?> getObjectType() {
-        return SandboxInterceptor.class;
-    }
+	/**
+	 * Resource containing the allowed definitions
+	 */
+	protected final Resource whitelist;
 
-    @Override
-    protected SandboxInterceptor createInstance() throws Exception {
-        if (sandboxEnabled && blacklistEnabled) {
-            try (InputStream is = blacklist.getInputStream()) {
-                Blacklist bl = new Blacklist(new InputStreamReader(is));
-                bl.setGetEnvWhitelistRegex(whitelistGetEnvRegex);
-                return new SandboxInterceptor(bl);
-            }
-        } else if(sandboxEnabled) {
-            PermitAllWhitelist permitAllWhitelist = new PermitAllWhitelist();
-            permitAllWhitelist.setGetEnvWhitelistRegex(whitelistGetEnvRegex);
-            return new SandboxInterceptor(permitAllWhitelist);
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * List of regex to allow System.getenv()
+	 */
+	protected final List<String> whitelistGetEnvRegex;
+
+	protected final List<Class<?>> extensions;
+
+	@ConstructorProperties({"sandboxEnabled", "blacklistEnabled", "blacklist",
+			"whitelistEnabled", "whitelist", "whitelistGetEnvRegex",
+			"extensions"})
+	public SandboxInterceptorFactory(final boolean sandboxEnabled, final boolean blacklistEnabled, final Resource blacklist,
+									 final boolean whitelistEnabled, final Resource whitelist, final String[] whitelistGetEnvRegex,
+									 final List<Class<?>> extensions) {
+		this.sandboxEnabled = sandboxEnabled;
+		this.blacklistEnabled = blacklistEnabled;
+		this.blacklist = blacklist;
+		this.whitelistEnabled = whitelistEnabled;
+		this.whitelist = whitelist;
+		if (blacklistEnabled && blacklist == null) {
+			throw new IllegalArgumentException("blacklistEnabled=true but 'blacklist' resource is null");
+		}
+		if (whitelistEnabled && whitelist == null) {
+			throw new IllegalArgumentException("whitelistEnabled=true but 'whitelist' resource is null");
+		}
+		this.whitelistGetEnvRegex = (whitelistGetEnvRegex == null) ? List.of() : Arrays.asList(whitelistGetEnvRegex);
+		this.extensions = emptyIfNull(extensions);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return SandboxInterceptor.class;
+	}
+
+	@Override
+	protected SandboxInterceptor createInstance() throws Exception {
+		if (!sandboxEnabled) {
+			return null;
+		}
+		return new SandboxInterceptor(getWhitelist(), extensions);
+	}
+
+	/**
+	 * Get a sandbox whitelist based on the current configuration:
+	 * <ul>
+	 * <li>If the sandbox is disabled, returns <code>null</code></li>
+	 * <li>If both the whitelist and blacklist are disabled, returns a <code>PermitAllWhitelist</code></li>
+	 * <li>If either (or both) the whitelist or blacklist are enabled, returns a <code>CompositeWhitelist</code> containing the enabled lists.
+	 * The resulting whitelist will allow a call if and only if it is allowed by both the whitelist and the blacklist</li>
+	 * </ul>
+	 *
+	 * @throws IOException if there is any error loading the whitelist/blacklist resources
+	 */
+	protected Whitelist getWhitelist() throws IOException {
+		if (!sandboxEnabled) {
+			// Nothing to do here
+			return null;
+		}
+		if (!blacklistEnabled && !whitelistEnabled) {
+			Whitelist whitelist = new PermitAllWhitelist();
+			whitelist.setGetEnvWhitelistRegex(whitelistGetEnvRegex);
+			return whitelist;
+		}
+
+		Collection<Whitelist> delegateLists = new ArrayList<>(2);
+		if (whitelistEnabled) {
+			try (InputStream is = whitelist.getInputStream()) {
+				Whitelist whitelist = new StaticWhitelist(new InputStreamReader(is));
+				whitelist.setGetEnvWhitelistRegex(whitelistGetEnvRegex);
+				delegateLists.add(whitelist);
+			}
+		}
+		if (blacklistEnabled) {
+			try (InputStream is = blacklist.getInputStream()) {
+				Blacklist blacklist = new Blacklist(new InputStreamReader(is));
+				blacklist.setGetEnvWhitelistRegex(whitelistGetEnvRegex);
+				delegateLists.add(blacklist);
+			}
+		}
+
+		return new CompositeWhitelist(delegateLists);
+	}
+
 
 }
